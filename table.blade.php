13793f1e44581907df0be9e9f553415ea7c86d74
KzOJT <150648757+youdipugo@users.noreply.github.com>
2023-12-13 15:19:23 +0800

    Add files via upload

---
+++ table.blade.php
@@ -0,0 +1,138 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <button class="backbtn">Back</button>
+<div class="wholetbl">   
+<table class="table">
+<thead>
+<th>CustomerCategory</th>
+<th>Lastname</th>
+<th>Firstname</th>
+<th>Middlename</th>
+<th>Email</th>
+<th>Address</th>
+<th>MobileNumber</th>
+<th>TelephoneNumber</th>
+<th>Companyname</th>
+
+</thead>
+<tbody>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+          
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+    <tr>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+</tbody>
+</table>
+</div>   
+</body>
+</html>
