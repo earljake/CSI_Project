14e1c20dc9f19ac9747deac3d1cea4727ee6fb68
BonBonOJT <daniel.ojtcsi@gmail.com>
2024-01-08 14:07:57 +0800

    Home

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="box">
+                <div class="navbar">
+                    <img src="css/convergelogo.png" id="logo">
+                </div>
+
+            <div class="wrapper">
+                <div class="left">
+                    <div class="leftlogo">
+                        <img src="css/convergelogo.png" id="leftlogo" >
+                        <p id="text">Monitoring System</p>
+                    </div>
+                    <div class="button">
+                        <button id="login">Log in</button>
+                        <button id="signup">Sign in</button>
+                    </div>
+                </div>
+
+                <div class="right">
+                    <img src="css/image.png" id="rightlogo">
+                </div>
+                
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
