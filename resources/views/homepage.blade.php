bfd856340be55630187a1fca96c70e4e281f3228
BonBonOJT <daniel.ojtcsi@gmail.com>
2024-01-18 16:20:11 +0800

    homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
+</head>
+<body>
+    <div class="wrapper">
+       
+        <div class="container">
+            <div class="left">
+                
+                <div class="sidebar">
+                    <ul>
+                        <div class="logo">
+                            <img src="/css/convergelogo.png" id="leftlogo">
+                        </div>
+                        <li><a href="#"><iconify-icon icon="mdi:user"></iconify-icon>Registration of Client</a></li>
+                        <li><a href="#"><iconify-icon icon="mdi:user-search"></iconify-icon>Client Masterlist</a></li>
+                        <li><a href="#"><iconify-icon icon="cil:chart-line"></iconify-icon>Marketing and Sales</a></li>
+                        <li><a href="#"><iconify-icon icon="material-symbols:engineering"></iconify-icon>Engineering</a></li>
+                    </ul>
+                </div>
+            
+            </div>
+            <div class="right">
+                <div class="csilogo"><img src="css/convergelogo.png" id="csilogo"></div>
+                <div class="undraw"><img src="css/undraw.png" id="undraw"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
